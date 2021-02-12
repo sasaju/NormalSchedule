a7615eb822c4c6116f9d3712135d88f2afed5447
sasaju <1289142675@qq.com>
2021-02-12 16:32:39 +0800

    基本完成登陆界面

--- app/src/main/java/com/liflymark/normalschedule/logic/network/ServiceCreator.kt
+++ app/src/main/java/com/liflymark/normalschedule/logic/network/ServiceCreator.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object ServiceCreator {
 
-    private const val BASE_URL = "http:///"
+    private const val BASE_URL = "http://ttt/"
 
     private val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
